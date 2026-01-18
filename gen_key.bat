da98e2dbcd435b7c74b3b6ae3de543b5631f3463
Fluxorae <fluxorae@gmail.com>
2026-01-18 18:20:28 +0530

    Clean deployment: Remove legacy files and configs for production

--- gen_key.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-if exist "C:\Users\fluxo\.ssh\id_rsa" del "C:\Users\fluxo\.ssh\id_rsa"
-if exist "C:\Users\fluxo\.ssh\id_rsa.pub" del "C:\Users\fluxo\.ssh\id_rsa.pub"
-ssh-keygen -t rsa -b 4096 -f C:\Users\fluxo\.ssh\id_rsa -N ""
